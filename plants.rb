807c38147b520184268ee723492d24fe3be8cf80
Allen Russell <garyrusselljr@gmail.com>
2023-04-12 16:26:35 -0600

    Change conditional

--- plants.rb
+++ plants.rb
@@ -1,7 +1,7 @@
-gets_sunshine = true
-ounces_of_water = 10
+gets_sunshine = false
+ounces_of_water = 9
 
-if gets_sunshine == true && ounces_of_water >= 10
+if gets_sunshine == true || ounces_of_water >= 10
     p "The plant is alive!"
 else
     p "You are the grim reaper of plants."
